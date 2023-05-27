34dc055da51dec32fb7d4313671cf96c52155ee6
Conkeegs <conkeegs@gmail.com>
2023-05-26 23:40:18 -0400

    deleting worlds working

--- src/main/java/com/conkeegs/truehardcore/TrueHardcore.java
+++ src/main/java/com/conkeegs/truehardcore/TrueHardcore.java
@@ -2,18 +2,26 @@ package com.conkeegs.truehardcore;
 
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.WorldOptions;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.event.TickEvent.ServerTickEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.server.ServerLifecycleHooks;
 import net.minecraft.network.chat.Component;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,6 +41,7 @@ public class TrueHardcore {
     // custom logga
     private static final Logger LOGGER = LoggerFactory.getLogger("[TRUEHARDCORE]");
 
+    private boolean shouldShutdownServer = false;
     public TrueHardcore() {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -42,15 +51,18 @@ public class TrueHardcore {
     public void onPlayerDeath(LivingDeathEvent event) {
         if (event.getEntity() instanceof ServerPlayer) {
             // ServerPlayer playerWhoDied = (ServerPlayer) event.getEntity();
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onServerTick(ServerTickEvent event) {
+        if (event.phase == Phase.END && shouldShutdownServer) {
             MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
-            List<ServerPlayer> playerList = new ArrayList<>(server.getPlayerList().getPlayers());
 
-            for (ServerPlayer player : playerList) {
-                player.connection
-                        .disconnect(Component.literal(event.getSource().getLocalizedDeathMessage(player).getString()));
+            try {
+                server.stopServer();
+            } catch (Exception exception) {
+                if (!(exception instanceof FileNotFoundException)) {
+                    LOGGER.error("Error shutting down server - ", exception.getMessage());
+                }
             }
-
-            server.stopServer();
 
             List<String> worldsToDelete = Arrays.asList("world", "world_nether", "world_the_end");
 
@@ -61,7 +73,7 @@ public class TrueHardcore {
                     try {
                         FileUtils.deleteDirectory(worldFolder);
 
-                        LOGGER.info("{} deleted successfully.", worldFolder.getName());
+                        LOGGER.info("World '{}' deleted successfully.", worldFolder.getName());
                     } catch (IOException ioe) {
                         LOGGER.error("Error deleting world folder - ", ioe.getMessage());
                     }
@@ -90,6 +102,26 @@ public class TrueHardcore {
             } catch (IOException ioe) {
                 LOGGER.error("Error writing to server properties file - ", ioe.getMessage());
             }
+
+            LOGGER.info("Server shut down successfully.");
+
+            shouldShutdownServer = false;
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerDeath(LivingDeathEvent event) {
+        if (event.getEntity() instanceof ServerPlayer) {
+            // ServerPlayer playerWhoDied = (ServerPlayer) event.getEntity();
+            MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+            List<ServerPlayer> playerList = new ArrayList<>(server.getPlayerList().getPlayers());
+
+            for (ServerPlayer player : playerList) {
+                player.connection
+                        .disconnect(Component.literal(event.getSource().getLocalizedDeathMessage(player).getString()));
+            }
+
+            shouldShutdownServer = true;
         }
     }
 
