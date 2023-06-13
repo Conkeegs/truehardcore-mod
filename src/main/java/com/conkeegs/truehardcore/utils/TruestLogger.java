35a3e64da4124c400e78399112b3bddad3d76ddb
Conkeegs <conkeegs@gmail.com>
2023-06-13 19:50:16 -0400

    got rid of log brackets

--- src/main/java/com/conkeegs/truehardcore/utils/TruestLogger.java
+++ src/main/java/com/conkeegs/truehardcore/utils/TruestLogger.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TruestLogger {
-    private static final Logger LOGGER = LoggerFactory.getLogger("[TRUEHARDCORE]");
+    private static final Logger LOGGER = LoggerFactory.getLogger("TRUEHARDCORE");
 
     public static Logger getLogger() {
         return LOGGER;
